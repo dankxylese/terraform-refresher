1076d66f4144fed9efa084c806ad6072af7affac
Vladyslav Kashtelyanov <darkxylese@slapmywang.com>
2023-09-07 10:30:05 +0100

    feat(tf): github resource creation and vars file

--- main.tf
+++ main.tf
@@ -1,16 +1,27 @@
 provider "aws" {
-  region = "eu-west-1"  # Change this to your desired region
+  region = var.aws-region # Change this to your desired region
 }
 
-resource "aws_instance" "example_instance" {
-  ami           = "ami-0136ddddd07f0584f"  # Change this to the AMI ID you want to use
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Vlads-test-instance"
-  }
+provider "github" {
+  token = var.github_token
 }
 
-output "instance_ip" {
-  value = aws_instance.example_instance.public_ip
+resource "github_repository" "test" {
+  name        = "repo-made-with-tf"
+  description = "This test repo was made with terraform"
+  visibility  = "public"
 }
+
+# resource "aws_instance" "example_instance" {
+#   ami           = "ami-0136ddddd07f0584f"  # Change this to the AMI ID you want to use
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "vlad-test-instance"
+#   }
+# }
+
+# output "instance_ip" {
+#   value = aws_instance.example_instance.public_ip
+# }
+
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws-region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Github access token"
+}
